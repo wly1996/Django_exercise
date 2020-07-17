1f2790a52f696674d0881fba0d2e3838463b497e
wly1996 <wanglingyu1996@126.com>
2020-07-17 15:57:13 +0800

    添加文章详情页，添加模版页，继承模板】

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.utils import timezone
 
 # Create your models here.
@@ -51,3 +52,6 @@ class Post(models.Model): #文章
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self): #获取文章详情
+        return reverse('blog:detail', kwargs = {'pk': self.pk})
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog' #定义显示的网站的名称
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('posts/<int:pk>/', views.detail, name = 'detail') #添加文章详情页
 ]
