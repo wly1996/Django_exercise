aa508e2c99d951eae3195ecd436e933c296f8eaa
wly1996 <wanglingyu1996@126.com>
2020-07-24 22:04:39 +0800

    显示每个分类下的文章数量

--- blog/templatetags/blog_extra.py
+++ blog/templatetags/blog_extra.py
@@ -3,6 +3,9 @@
 from django import template
 from ..models import Post, Category, Tag
 
+from django.db.models.aggregates import Count
+from blog.models import Category
+
 register = template.Library()
 
 @register.inclusion_tag('blog/inclusions/_recent_posts.html', takes_context = True) #最新文章模板标签
@@ -15,9 +18,10 @@ def show_archives(context):
 
 @register.inclusion_tag('blog/inclusions/_categories.html', takes_context = True) #分类模板标签
 def show_categories(context):
-    return{'category_list': Category.objects.all(),}
+    category_list = Category.objects.annotate(num_posts = Count('post')).filter(num_posts__gt = 0) #从数据库中取出所有文章并进行计数
+    return{'category_list': category_list,}
     
 @register.inclusion_tag('blog/inclusions/_tags.html', takes_context = True) #标签云模板标签
 def show_tags(context):
+    tag_list = Tag.objects.annotate(num_posts = Count('post')).filter(num_posts__gt = 0) #对Tag进行同样的操作
     return{'tag_list': Tag.objects.all(),}
-
